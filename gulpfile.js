b55e3c3797780d26b234a36291dcdfa9203c8d65
Polina Obertysheva <PolinaObertysheva@MacBook-Pro-de-Polina.local>
2017-12-22 21:37:33 +0100

    initial html css

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const Gulp = require('gulp');
+const Sass = require('gulp-sass');
+const PostCSS = require('gulp-postcss');
+const CSSNext = require('postcss-cssnext');
+const CleanCSS = require('gulp-clean-css');
+const Concat = require('gulp-concat');
+const Uglify = require('gulp-uglify');
+const BrowserSync = require('browser-sync').create();
+
+Gulp.task('sass', () => Gulp.src([
+  // Example to add CSS dependencies from npm (after doing `npm install bootstrap`):
+  // (to get the /dist/... path, just look into the corresponding "node_modules" folder)
+  require.resolve('bootstrap/dist/css/bootstrap.min.css'), './scss/**/*.scss'
+])
+  .pipe(Sass().on('error', Sass.logError))
+  .pipe(PostCSS([ CSSNext() ]))
+  .pipe(Concat('style.css'))
+  .pipe(CleanCSS({ compatibility: 'ie10' }))
+  .pipe(Gulp.dest('./public'))
+  .pipe(BrowserSync.stream())
+);
+
+Gulp.task('js', () => Gulp.src([
+  // Example to add dependencies from npm (after doing `npm install jquery` and `npm install bootstrap`):
+  // (to get the /dist/... path, just look into the corresponding "node_modules" folder)
+  require.resolve('jquery/dist/jquery.slim.min'),
+  require.resolve('bootstrap/dist/js/bootstrap'),
+  './js/**/*.js',
+])
+  .pipe(Concat('bundle.js'))
+  .pipe(Uglify())
+  .pipe(Gulp.dest('./public'))
+  .pipe(BrowserSync.stream())
+);
+
+Gulp.task('browser-sync', () => BrowserSync.init({
+  server: { baseDir: 'public' }
+}));
+
+Gulp.task('noop', () => undefined);
+
+Gulp.task('default', ['js', 'sass', 'browser-sync'], () => {
+  Gulp.watch('./scss/**/*.scss', ['sass']).on('change', BrowserSync.reload);
+  Gulp.watch('./public/**/*.html', ['noop']).on('change', BrowserSync.reload);
+  Gulp.watch('./js/**/*.js', ['js']);
+});
